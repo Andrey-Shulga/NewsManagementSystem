f7b21f573790073b4aba05001f5653b6592b8895
Andre Shulga <solyariz@gmail.com>
2017-05-11 16:54:46 +0600

    added test for CRUD operations

--- src/test/java/com/epam/testapp/dao/DaoTest.java
+++ src/test/java/com/epam/testapp/dao/DaoTest.java
@@ -70,6 +70,34 @@ public class DaoTest {
         assertEquals(news.getId(), result.getId());
     }
 
+    @Test
+    public void testEditNews() {
+
+        News news = createTestNews();
+        String oldTitle = "oldTestTitle";
+        String newTitle = "newTestTitle";
+        news.setTitle(oldTitle);
+        News savedNews = newsDao.save(news);
+        assertEquals(oldTitle, savedNews.getTitle());
+        savedNews.setTitle(newTitle);
+        newsDao.save(savedNews);
+        News result = newsDao.findById(News.class, savedNews.getId());
+        assertEquals(newTitle, result.getTitle());
+
+    }
+
+    @Test
+    public void deleteNews() {
+
+        News news = createTestNews();
+        News savedNews = newsDao.save(news);
+        assertNotNull(savedNews);
+        newsDao.delete(savedNews);
+        News result = newsDao.findById(News.class, savedNews.getId());
+        assertNull(result);
+
+    }
+
     private News createTestNews() {
         return new News("testTitle", new Date(), "testBrief", "testContent");
     }
