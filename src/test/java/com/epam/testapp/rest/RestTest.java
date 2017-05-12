ee714166e00b14518eaa31d6833e7e9efea1e762
Andre Shulga <solyariz@gmail.com>
2017-05-12 15:05:53 +0600

    added test for REST saveNews

--- src/test/java/com/epam/testapp/rest/RestTest.java
+++ src/test/java/com/epam/testapp/rest/RestTest.java
@@ -1,9 +1,11 @@
 package com.epam.testapp.rest;
 
+import com.epam.testapp.model.News;
 import com.epam.testapp.util.DateConverter;
 import com.github.springtestdbunit.DbUnitTestExecutionListener;
 import com.github.springtestdbunit.TransactionDbUnitTestExecutionListener;
 import com.github.springtestdbunit.annotation.DatabaseSetup;
+import org.codehaus.jackson.map.ObjectMapper;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -20,9 +22,12 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.context.WebApplicationContext;
 
+import java.util.Date;
+
 import static com.jayway.jsonassert.impl.matcher.IsCollectionWithSize.hasSize;
 import static org.hamcrest.CoreMatchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @ContextConfiguration(locations = {"classpath:testAppContext.xml", "classpath:dispatcher-servlet.xml"})
@@ -68,12 +73,37 @@ public class RestTest {
     @DatabaseSetup("/sampleData.xml")
     public void getNewsByIdTest() throws Exception {
 
-        mockMvc.perform(get("/rest/news/get/{id}", 1L))
+        final long testId = 1L;
+        mockMvc.perform(get("/rest/news/get/{id}", testId))
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8_VALUE))
-                .andExpect(jsonPath("$.id", is(1)))
+                .andExpect(jsonPath("$.id", is(testId)))
                 .andExpect(jsonPath("$.title", is("testTitle")))
                 .andExpect(jsonPath("$.date", is(DateConverter.getStrDateToLong("2017-05-06 17:31:32"))))
+                .andExpect(jsonPath("$.brief", is("testBrief")))
+                .andExpect(jsonPath("$.content", is("testContent")));
+    }
+
+    @Test
+    public void saveNews() throws Exception {
+
+        final int testId = 1;
+        final long testDate = 1494569142000L;
+        final News news = new News("testTitle", new Date(testDate), "testBrief", "testContent");
+        final ObjectMapper mapper = new ObjectMapper();
+
+        mockMvc.perform(get("/rest/news/get/{id}", testId))
+                .andExpect(content().string("null"));
+
+        mockMvc.perform(post("/rest/news/save")
+                .contentType(MediaType.APPLICATION_JSON_UTF8_VALUE)
+                .content(mapper.writeValueAsString(news)))
+
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8_VALUE))
+                .andExpect(jsonPath("$.id", is(testId)))
+                .andExpect(jsonPath("$.title", is("testTitle")))
+                .andExpect(jsonPath("$.date", is(testDate)))
                 .andExpect(jsonPath("$.brief", is("testBrief")))
                 .andExpect(jsonPath("$.content", is("testContent")));
     }
